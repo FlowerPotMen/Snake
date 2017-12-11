5cbaace02a23e8f34c862802b79da444822de4ca
luke <luke@HomePC.lan>
2017-12-11 11:33:50 +0000

    added snake body and food as well as colours

---
+++ snake/public_html/Sprite.js
@@ -0,0 +1,80 @@
+function Sprite(colour, radius, x, y, shape) {
+
+    this.colour = colour;
+    this.radius = radius;
+    this.x = x;
+    this.y = y;
+    this.shape = shape;
+    this.div = document.createElement("DIV");
+
+
+    this.init = function () {
+
+        this.div.style.position = "absolute";
+        this.div.style.left = this.x;
+        this.div.style.top = this.y;
+
+        var c = document.createElement("CANVAS");
+
+        var ctx = c.getContext("2d");
+        if (this.shape == "square") {
+            ctx.rect(0, 0, this.radius, this.radius
+
+                    );
+            ctx.fillStyle = this.colour;
+        } else if (this.shape == "circle") {
+            ctx.beginPath();
+            ctx.arc(this.radius, this.radius, this.radius, 0, 2 * Math.PI);
+            ctx.fillStyle = this.colour;
+        }
+
+
+        ctx.fill();
+        ctx.stroke();
+
+        this.div.appendChild(c);
+
+        document.body.appendChild(this.div);
+
+    };
+    this.setPosition = function (lng, lat) {
+        this.x = lng;
+        this.y = lat;
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+    }
+
+    this.move = function (howFar, dir) {
+        //it moves the sprite on the screen how far and in this direction
+
+
+        if (dir == 0) {
+            this.x = this.x - howFar;
+
+        } else if (dir == 1) {
+            this.x = this.x + howFar;
+        } else if (dir == 2) {
+            this.y = this.y - howFar;
+        } else if (dir == 3) {
+            this.y = this.y + howFar;
+        }
+
+
+        //test if we have hit the edge
+        if (this.x <= 0 || this.x >= window.innerWidth || this.y <= 0 || this.y >= window.innerHeight) {
+
+
+            alert("you have died :(")
+            this.x = window.innerWidth / 2;
+            this.y = window.innerHeight / 2;
+        }
+        this.div.style.left = this.x + "px";
+        this.div.style.top = this.y + "px";
+
+        checkContact();
+    }
+    this.die = function () {
+        document.body.removeChild(this.div);
+    }
+
+}
--- snake/public_html/controller.js
+++ snake/public_html/controller.js
@@ -1,96 +1,110 @@
 //globals
 var circles = [];
+var direction = [];
+var food;
+var snakeLength = 7;
+var distance = 20;
+var current = 0;
+var colour=["red","orange","yellow","green","blue","purple","indigo"];
 
-//var d = document.createElement("DIV");
-//var d2 = document.createElement("DIV");
+function run() {
+    var x = window.innerWidth / 2;
+    var y = window.innerHeight / 2;
 
-var speed = 25;
-var distance = 10;
+    // creats new food
+    spawnFood();
+    // creats circles 
 
-var current=0;
-
-function run(){
-    circles.push(drawCircle("yellow",11,40,50));
-    circles.push(drawCircle("red",11,80,50));
-    circles.push(drawCircle("blue",11,120,50));
-    circles.push(drawCircle("green",11,160,50));
-}
-
-
-
-function drawCircle(colour,radius,x,y){
-    var div = document.createElement("DIV");
-    div.style.position="absolute";
-    div.style.left=x;
-    div.style.top=y;
-    
-    var c = document.createElement("CANVAS");
-
-    var ctx = c.getContext("2d");
-    ctx.beginPath();
-    ctx.arc(radius,radius,radius,0,2*Math.PI);
-    ctx.fillStyle = colour;
-    ctx.fill();
-    ctx.stroke();
-    
-    div.appendChild(c);
-    
-    document.body.appendChild(div);
-    return div;
-}
-function keys(event){
-    //w=119 s=115 a=97 d=100 enter=13 space=32
-    
-    var e = event.keyCode;
-    
-  //alert(e)
-    
-   if(e=="100"){
-       move(speed,"right");
-    }
-    else if(e=="119"){
-        move(speed,"up");
-    }
-    else if(e=="115"){
-        move(speed,"down");
-    }
-    else if(e=="97"){
-        move(speed,"left");
-   }
-   else if(e=="32"){
-       current++;
-       if(current>(circles.length-1)){
-           current=0;
-       }
-
+    for (c = 0; c < snakeLength; c++) {
        
-   }
-}
-function move(howFar,direction){
-    
-    d=circles[current];
-    
-    var x=parseInt(d.style.left);
-    var y=parseInt(d.style.top);
-    
-    
-    if(direction=="left"){
-        x=x-howFar;
-        
-    }
-    else if(direction=="right"){
-        x=x+howFar;
-    }
-    else if(direction=="up"){
-        y=y-howFar;
-    }
-    else if(direction=="down"){
-        y=y+howFar;
-    }
-    d.style.left=x+"px";
-    d.style.top=y+"px";
-}
-    
-    
-   
+        circles[c] = new Sprite(colour[c], 11, randomNumber(0, (window.innerWidth - 330)), randomNumber(0, (window.innerHeight - 330)), "circle");
 
+        circles[c].init();
+        
+
+    }
+    //alert(circles.length);
+
+    direction.push(randomNumber(0, 4));
+
+
+
+    setInterval(function () {
+        for (i = 0; i < circles.length; i++) {
+            currentX = circles[i].x;
+            currentY = circles[i].y;
+            if (i == 0) {
+                circles[i].move(distance, direction[i]);
+            } else {
+                circles[i].setPosition(nextX, nextY);
+            }
+            nextX = currentX;
+            nextY = currentY;
+        }
+    }, 50);
+}
+
+
+
+
+function keys(event) {
+    //w=119 s=115 a=97 d=100 enter=13 space=32
+
+    var e = event.keyCode;
+
+    //alert(e)
+
+    if (e == "100" || e == "68") {
+        // move(speed, "right");
+        direction[current] = 1;
+    } else if (e == "119" || e == "87") {
+        //move(speed, "up");
+        direction[current] = 2
+    } else if (e == "115" || e == "83") {
+        //move(speed, "down");
+        direction[current] = 3;
+    } else if (e == "97" || e == "65") {
+        //move(speed, "left");
+        direction[current] = 0;
+    } else if (e == "32") {
+        current++;
+        if (current > (circles.length - 1)) {
+            current = 0;
+        }
+
+
+    }
+}
+
+function randomNumber(min, max) {
+
+    return Math.floor((Math.random() * (max - min))) + min;
+
+}
+//checks if any of the sprites are touching
+function checkContact() {
+    var x1 = circles[0].x;
+    var y1 = circles[0].y;
+    var x2 = food.x;
+    var y2 = food.y;
+    var z1 = circles[0].radius;
+    var z2 = food.radius;
+
+    if ((x1 >= x2 && x1 <= (x2 + z2) &&
+            y1 >= y2 && y1 <= (y2 + z2)) ||
+            ((x1 + z1) >= x2 && (x1 + z1) <= (x2 + z2) &&
+                    (y1 + z1) >= y2 && (y1 + z1) <= (y2 + z2))
+            ) {
+        food.die();
+        distance = distance + 2;
+        spawnFood();
+    }
+}
+
+
+//spawns new food
+
+function spawnFood() {
+    food = new Sprite("red", 22, randomNumber(0, (window.innerWidth - 11)), randomNumber(0, (window.innerHeight - 11)), "square");
+    food.init();
+}
