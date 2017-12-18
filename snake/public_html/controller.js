6146b5fec0e67bf497bbbc38a4d490b45d91c173
luke <luke@HomePC.lan>
2017-12-18 11:34:06 +0000

    added the snake getting larger and checks if it goes back on its self

--- snake/public_html/controller.js
+++ snake/public_html/controller.js
@@ -1,11 +1,11 @@
 //globals
 var circles = [];
-var direction = [];
+var direction;
 var food;
-var snakeLength = 7;
+var snakeLength = 1;
 var distance = 20;
 var current = 0;
-var colour=["red","orange","yellow","green","blue","purple","indigo"];
+var colour = "yellow";
 
 function run() {
     var x = window.innerWidth / 2;
@@ -13,35 +13,38 @@ function run() {
 
     // creats new food
     spawnFood();
-    // creats circles 
 
+    // creats circles 
     for (c = 0; c < snakeLength; c++) {
-       
-        circles[c] = new Sprite(colour[c], 11, randomNumber(0, (window.innerWidth - 330)), randomNumber(0, (window.innerHeight - 330)), "circle");
+
+        circles[c] = new Sprite(colour, 11, randomNumber(0, (window.innerWidth - 330)), randomNumber(0, (window.innerHeight - 330)), "circle");
 
         circles[c].init();
-        
+
 
     }
     //alert(circles.length);
 
-    direction.push(randomNumber(0, 4));
+    direction = randomNumber(0, 4);
 
 
-
+    //keeps snake moving
     setInterval(function () {
         for (i = 0; i < circles.length; i++) {
             currentX = circles[i].x;
             currentY = circles[i].y;
             if (i == 0) {
-                circles[i].move(distance, direction[i]);
+                circles[i].move(distance, direction);
             } else {
                 circles[i].setPosition(nextX, nextY);
             }
             nextX = currentX;
             nextY = currentY;
         }
-    }, 50);
+        for (z = 1; z < circles.length; z++){
+            checkOverlap(circles[0],circles[z]);
+        }
+    }, 100);
 }
 
 
@@ -56,23 +59,29 @@ function keys(event) {
 
     if (e == "100" || e == "68") {
         // move(speed, "right");
-        direction[current] = 1;
-    } else if (e == "119" || e == "87") {
-        //move(speed, "up");
-        direction[current] = 2
-    } else if (e == "115" || e == "83") {
-        //move(speed, "down");
-        direction[current] = 3;
-    } else if (e == "97" || e == "65") {
-        //move(speed, "left");
-        direction[current] = 0;
-    } else if (e == "32") {
-        current++;
-        if (current > (circles.length - 1)) {
-            current = 0;
+        if (direction != 0 || snakeLength == 1) {
+
+            direction = 1;
         }
 
+    } else if (e == "119" || e == "87") {
+        //move(speed, "up");
+        if (direction != 3 || snakeLength == 1) {
 
+            direction = 2;
+        }
+    } else if (e == "115" || e == "83") {
+        //move(speed, "down");
+        if (direction != 2 || snakeLength == 1) {
+
+            direction = 3;
+        }
+    } else if (e == "97" || e == "65") {
+        //move(speed, "left");
+        if (direction != 1 || snakeLength == 1) {
+
+            direction = 0;
+        }
     }
 }
 
@@ -96,8 +105,26 @@ function checkContact() {
                     (y1 + z1) >= y2 && (y1 + z1) <= (y2 + z2))
             ) {
         food.die();
+        increaseLength(2);
         distance = distance + 2;
         spawnFood();
+    }
+}
+//checks if snake goes back on its self
+function checkOverlap(c1, c2) {
+    var x1 = c1.x;
+    var y1 = c1.y;
+    var x2 = c2.x;
+    var y2 = c2.y;
+    var z1 = c1.radius;
+    var z2 = c2.radius;
+
+    if ((x1 >= x2 && x1 <= (x2 + z2) &&
+            y1 >= y2 && y1 <= (y2 + z2)) ||
+            ((x1 + z1) >= x2 && (x1 + z1) <= (x2 + z2) &&
+                    (y1 + z1) >= y2 && (y1 + z1) <= (y2 + z2))
+            ) {
+        alert("hi");
     }
 }
 
@@ -107,4 +134,17 @@ function checkContact() {
 function spawnFood() {
     food = new Sprite("red", 22, randomNumber(0, (window.innerWidth - 11)), randomNumber(0, (window.innerHeight - 11)), "square");
     food.init();
+}
+function increaseLength(howMany) {
+    for (c = 0; c < howMany; c++) {
+
+        var circ = new Sprite(colour, 11, randomNumber(0, (window.innerWidth - 330)), randomNumber(0, (window.innerHeight - 330)), "circle");
+
+        circ.init();
+
+        circles.push(circ);
+
+
+    }
+    snakeLength = snakeLength + howMany;
 }
