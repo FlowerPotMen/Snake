840085d9e644ae5a529568fa1041874064ee6674
luke <luke@HomePC.lan>
2018-06-11 11:29:37 +0100

    created visible grid

--- Tetris/public_html/Block.js
+++ Tetris/public_html/Block.js
@@ -1,7 +1,7 @@
 function Block() {
     this.div = document.createElement("DIV");
     this.size = 20;
-    this.shapes = ['L', 'Square', 'ZigZag'];
+    this.shapes = ['Line', 'L', 'Square', 'ZigZag'];
     this.colours = ['blue', 'red', 'green', 'yellow', 'purple'];
     this.ctx;
     this.spin = 0;
@@ -11,8 +11,8 @@ function Block() {
     this.height;
 
     this.move = function (direction, distance) {
-        if(distance==null){
-            distance=this.size;
+        if (distance == null) {
+            distance = this.size;
         }
         if (direction == "down") {
             this.top = this.top + distance;
@@ -29,15 +29,16 @@ function Block() {
             blockHeight = this.height;
             blockWidth = this.width;
         }
-        //test if we have hit the edge
-        if ((this.top + blockHeight) >= window.innerHeight) {
+        
+        if ((this.top + blockHeight) > grid.height) {
+            //test if we have hit the bottom^^
             this.top = this.top - this.size;
             return 1;
-        }
-        else if ((this.left + blockWidth) >= (window.innerWidth / 2)+(this.size * 10)) {
+        } else if ((this.left + blockWidth) >= (window.innerWidth / 2) + (this.size * 11)) {
+            //tests if we have hit the right side^^ 
             this.left = this.left - this.size;
-        }
-        else if (this.left <= ((window.innerWidth / 2) - (this.size * 10))) {
+        } else if (this.left < ((window.innerWidth / 2) - (this.size * 11))) {
+            //tests if we have hit the left side^^
             this.left = this.left + this.size;
         }
         this.div.style.top = this.top;
@@ -49,17 +50,24 @@ function Block() {
             this.spin = 0;
         }
         this.div.style.transform = "rotate(" + this.spin + "deg)";
-        
+
         //if roated with odd height then adjust
-        if((this.spin==90 || this.spin==270) && (this.blocksHigh % 2)){
-            this.move("left", (this.size/2));
+        if ((this.spin == 90 || this.spin == 270) && (this.blocksHigh % 2)) {
+            console.log('hi');
+            this.move("left", (this.size / 2));
+            this.move("down", ((0 - this.size) / 2));
+
+        } else if ((this.spin == 0 || this.spin == 180) && (this.blocksHigh % 2)) {
+            this.move("right", (this.size / 2));
+            this.move("down", ((0 - this.size) / 2));
         }
     }
     this.spawn = function () {
         //selects a random shape and colour
         //display on screen
+        [this.top,this.left] = grid.cellLoc(11,0);
         this.div.style.position = "absolute";
-        this.left = window.innerWidth * 0.5;
+
         this.div.style.left = this.left;
         this.div.style.top = this.top;
 
@@ -77,6 +85,8 @@ function Block() {
             this.Square(this.ctx, this.colours[colour]);
         } else if (this.shapes[shape] == 'ZigZag') {
             this.ZigZag(this.ctx, this.colours[colour]);
+        } else if (this.shapes[shape] == 'Line') {
+          this.Line(this.ctx, this.colours[colour]);
         }
         this.div.appendChild(c);
         document.body.appendChild(this.div);
@@ -86,7 +96,7 @@ function Block() {
 
     }
     this.L = function (ctx, colour) {
-        
+
         this.blocksWide = 2;
         this.blocksHigh = 3;
 
@@ -103,10 +113,10 @@ function Block() {
         this.div.style.width = this.width;
     }
     this.Square = function (ctx, colour) {
-        
+
         this.blocksWide = 2;
         this.blocksHigh = 2;
-        
+
         ctx.rect(0, 0, this.size, this.size);
         ctx.rect(0, this.size, this.size, this.size);
         ctx.rect(this.size, 0, this.size, this.size);
@@ -120,14 +130,30 @@ function Block() {
         this.div.style.width = this.width;
     }
     this.ZigZag = function (ctx, colour) {
-        
+
         this.blocksWide = 2;
         this.blocksHigh = 3;
-        
+
         ctx.rect(0, 0, this.size, this.size);
         ctx.rect(0, this.size, this.size, this.size);
         ctx.rect(this.size, this.size, this.size, this.size);
         ctx.rect(this.size, this.size * 2, this.size, this.size);
+        ctx.fillStyle = colour;
+        ctx.fill();
+        ctx.stroke();
+        this.width = this.size * this.blocksWide;
+        this.height = this.size * this.blocksHigh;
+        this.div.style.height = this.height;
+        this.div.style.width = this.width;
+    }
+    this.Line = function (ctx, colour) {
+        this.blocksWide = 4;
+        this.blocksHigh = 1;
+
+        ctx.rect(0, 0, this.size, this.size);
+        ctx.rect(this.size, 0, this.size, this.size);
+        ctx.rect(this.size * 2, 0, this.size, this.size);
+        ctx.rect(this.size * 3, 0, this.size, this.size);
         ctx.fillStyle = colour;
         ctx.fill();
         ctx.stroke();
